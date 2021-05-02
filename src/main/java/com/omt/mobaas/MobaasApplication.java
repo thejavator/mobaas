993d41180bae75288c1e6f7526fbd003bbc444dd
El Mehdi TAHRI <elmehditahri@gmail.com>
2021-05-02 13:12:33 +0000

    add swagger to the project

--- src/main/java/com/omt/mobaas/MobaasApplication.java
+++ src/main/java/com/omt/mobaas/MobaasApplication.java
@@ -2,10 +2,9 @@ package com.omt.mobaas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
-@RestController
 public class MobaasApplication {
 
     public static void main(String[] args) {
